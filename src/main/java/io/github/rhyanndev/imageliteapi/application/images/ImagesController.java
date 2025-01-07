11f9145cc8f2d4350d11b50813df763f1054d492
rhyanndev <rhyannjosebezerra@gmail.com>
2025-01-07 20:08:54 -0300

    refactor: settings in image extension types and add service layer in controller

--- src/main/java/io/github/rhyanndev/imageliteapi/application/images/ImagesController.java
+++ src/main/java/io/github/rhyanndev/imageliteapi/application/images/ImagesController.java
@@ -1,30 +1,46 @@
 package io.github.rhyanndev.imageliteapi.application.images;
 
+import io.github.rhyanndev.imageliteapi.domain.entity.Image;
+import io.github.rhyanndev.imageliteapi.domain.enums.ImageExtension;
+import io.github.rhyanndev.imageliteapi.domain.service.ImageService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/v1/images")
 @Slf4j
+@RequiredArgsConstructor
 public class ImagesController {
 
+    private final ImageService service;
 
     @PostMapping
     public ResponseEntity save(
             @RequestParam(value = "file", required = false) MultipartFile file,
             @RequestParam("name") String name,
             @RequestParam("tags") List<String> tags
-    ) {
+    ) throws IOException {
         log.info("Imagem recebida: name: {}, size: {}", file.getOriginalFilename(), file.getSize());
-        log.info("Nome definido para a imagem: {}", name);
-        log.info("Tags: {}", tags);
+
+        Image image  = Image.builder()
+                .name(name)
+                .tags(String.join(",", tags))
+                .size(file.getSize())
+                .extension(ImageExtension.valueOf(MediaType.valueOf(file.getContentType())))
+                .file(file.getBytes())
+                .build();
+
+        service.save(image);
+
         return ResponseEntity.ok().build();
 
     }
--- src/main/java/io/github/rhyanndev/imageliteapi/domain/enums/ImageExtension.java
+++ src/main/java/io/github/rhyanndev/imageliteapi/domain/enums/ImageExtension.java
@@ -1,9 +1,26 @@
 package io.github.rhyanndev.imageliteapi.domain.enums;
 
+import org.springframework.http.MediaType;
+
+import java.util.Arrays;
+
 public enum ImageExtension {
-    PNG,
-    JPG,
-    GIF,
-    JPEG,
-    RAW
+
+    PNG(MediaType.IMAGE_PNG),
+    GIF(MediaType.IMAGE_GIF),
+    JPEG(MediaType.IMAGE_JPEG),
+    WEBP(MediaType.valueOf("image/webp"));
+
+    private MediaType mediaType;
+
+    ImageExtension(MediaType mediaType) {
+        this.mediaType = mediaType;
+    }
+
+    public static ImageExtension valueOf(MediaType mediaType){
+        return Arrays.stream(values())
+                .filter(ie -> ie.mediaType.equals(mediaType))
+                .findFirst()
+                .orElse(null);
+    }
 }
